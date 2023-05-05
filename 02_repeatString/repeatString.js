80be8dbe6f18fc0813da7aab21d318aa023dae96
mellisa <123798125+MariannaCross@users.noreply.github.com>
2023-05-05 14:11:19 -0700

    add return instead of logging

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,7 +3,7 @@ const repeatString = function(string,num) {
     for (let i = 0; i < num; i++) {
         result += string;
     }
-    console.log(result);
+    return(result);
 }
 repeatString("hey",3);
 
