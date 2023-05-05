6feb3e5a970ba145eda0c24e63222aff42ab6b48
mellisa <123798125+MariannaCross@users.noreply.github.com>
2023-05-05 13:51:08 -0700

    adding for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function() {
+    for (let i = 0; i<3; i++) {
+        console.log('hey');
+    }
 
-};
+}
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
