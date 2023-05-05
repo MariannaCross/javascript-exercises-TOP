8f0e9d478d7e4e31e1e866b240c176c7be293aee
mellisa <123798125+MariannaCross@users.noreply.github.com>
2023-05-05 14:45:59 -0700

    repeatStrings test complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,3 +1,5 @@
+const number = Math.floor(Math.random() * 1000)
+
 const repeatString = function(string,num) {
     if (num < 0 || !Number.isInteger(num)) {//returns ERROR if num is negative or non-integer
         return 'ERROR';
@@ -8,7 +10,7 @@ const repeatString = function(string,num) {
     }
     return(result);
 }
-repeatString("hey",-1);
+repeatString("", number);
 
 // Do not edit below this line
 module.exports = repeatString;
